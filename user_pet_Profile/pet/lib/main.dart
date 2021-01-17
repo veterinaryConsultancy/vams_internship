571638de1d26475092c2599bec332e033c92705c
Ashna Nizam <68196153+AshNiz24@users.noreply.github.com>
2021-01-17 09:36:08 +0530

    Update main.dart

--- user_pet_Profile/pet/lib/main.dart
+++ user_pet_Profile/pet/lib/main.dart
@@ -1,14 +1,12 @@
 ///The main class of the application from where the application starts
 import 'package:flutter/material.dart';
-//import 'user_profile.dart';
 import 'pet_profile.dart';
 
 void main() {
   runApp(
     MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: PetDetailsScreen(),
-      //UserProfile(),
+      home: PetDetailScreen(),
     ),
   );
 }
